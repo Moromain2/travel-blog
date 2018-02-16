11aca031de5bc6ad5bd94e75acf9a9bcae11fd47
Romain Mosser <mosser.romain@gmail.com>
2018-02-16 14:04:51 +1100

    migrations

--- db/migrate/20180210044653_add_summary_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddSummaryToPosts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :posts, :summary, :text_area
-  end
-end
--- db/migrate/20180210050747_remove_summary_from_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSummaryFromPosts < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :posts, :summary, :text_area
-  end
-end
